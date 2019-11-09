185eebdfd3faae87f91e8cee56f53d525730e73d
unknown <facundogallobarrosudovich@hotmail.com>
2019-11-09 17:40:21 -0300

    Terminado el método de selección de las tablas

--- src/grafica/Window_addHost.java
+++ src/grafica/Window_addHost.java
@@ -8,8 +8,11 @@ package grafica;
 import static grafica.Index.color_textHint;
 import java.awt.Color;
 import java.sql.Date;
+import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 import logica.SQL.SQLReserva;
 import logica.SQL.SQLTurista;
 import logica.Turista;
@@ -22,17 +25,30 @@ public class Window_addHost extends javax.swing.JFrame {
 
     String view;
     Turista turista;
+    int codigo;
 
-    public Window_addHost() {
+    public Window_addHost(int codigo) {
         initComponents();
         this.view = "ADDHOST";
+        this.codigo=codigo;
     }
 
     public void build() {
 
         this.setSize(900, 500);
         this.setLocationRelativeTo(null);
-
+        paintTabla(new SQLTurista().selectCheckIn(codigo),this.view);
+    }
+    public static void paintPanel(JPanel panel, JLayeredPane lyrPane) { //Coloca un el panel deseado en el JLayeredPane
+        lyrPane.removeAll();
+        lyrPane.add(panel);
+        lyrPane.repaint();
+        lyrPane.revalidate();
+    }
+        public static void paintTabla(DefaultTableModel modelo, String view) {
+        Tabla tabla = new Tabla(view);
+        tabla.buildTable(modelo);
+        paintPanel(tabla, layerTabla);
     }
 
     public void textHint(String txt, JTextField obj) { //Procedimiento para textHints
@@ -75,8 +91,7 @@ public class Window_addHost extends javax.swing.JFrame {
         fieldCodigo = new javax.swing.JFormattedTextField();
         lblCI1 = new javax.swing.JLabel();
         lblTitle = new javax.swing.JLabel();
-        scroll = new javax.swing.JScrollPane();
-        tabla = new javax.swing.JTable();
+        layerTabla = new javax.swing.JLayeredPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -250,21 +265,21 @@ public class Window_addHost extends javax.swing.JFrame {
         panel.add(lblTitle);
         lblTitle.setBounds(360, 0, 180, 40);
 
-        tabla.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        scroll.setViewportView(tabla);
+        layerTabla.setPreferredSize(new java.awt.Dimension(936, 286));
 
-        panel.add(scroll);
-        scroll.setBounds(430, 70, 440, 340);
+        javax.swing.GroupLayout layerTablaLayout = new javax.swing.GroupLayout(layerTabla);
+        layerTabla.setLayout(layerTablaLayout);
+        layerTablaLayout.setHorizontalGroup(
+            layerTablaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 450, Short.MAX_VALUE)
+        );
+        layerTablaLayout.setVerticalGroup(
+            layerTablaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 420, Short.MAX_VALUE)
+        );
+
+        panel.add(layerTabla);
+        layerTabla.setBounds(440, 60, 450, 420);
 
         getContentPane().add(panel);
         panel.setBounds(0, 0, 900, 500);
@@ -369,12 +384,12 @@ public class Window_addHost extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    /*public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -394,13 +409,13 @@ public class Window_addHost extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
 
-        /* Create and display the form */
+        /* Create and display the form 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Window_addHost().setVisible(true);
             }
         });
-    }
+    }*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIngresar;
@@ -414,6 +429,7 @@ public class Window_addHost extends javax.swing.JFrame {
     public javax.swing.JTextField fieldNombre;
     public javax.swing.JTextField fieldNumero;
     public javax.swing.JFormattedTextField fieldTelefono;
+    private static javax.swing.JLayeredPane layerTabla;
     private javax.swing.JLabel lblApellido;
     private javax.swing.JLabel lblCI;
     private javax.swing.JLabel lblCI1;
@@ -424,7 +440,5 @@ public class Window_addHost extends javax.swing.JFrame {
     private javax.swing.JLabel lblTitle;
     private javax.swing.JPanel panel;
     private javax.swing.JPanel panelAdd;
-    private javax.swing.JScrollPane scroll;
-    private javax.swing.JTable tabla;
     // End of variables declaration//GEN-END:variables
 }
